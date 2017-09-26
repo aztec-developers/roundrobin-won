3a15d410c512844bd441cf0a112034229fc63023
Sergio <Sergio.valencia619@gmail.com>
2017-09-25 20:45:14 -0700

    round robin print names

--- round_robin.py
+++ round_robin.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python
+import csv
+with open('names.csv', 'rb') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter= ' ', quotechar='|')
+	for row in spamreader:
+		print ', '.join(row)
